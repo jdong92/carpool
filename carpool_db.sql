91cfb72694522e97bace2c884a4b6166c7a0f6eb
mawilson <mawilson2012@gmail.com>
2012-11-19 21:26:06 -0800

    Update carpool_db.sql

--- carpool_db.sql
+++ carpool_db.sql
@@ -1,92 +1,229 @@
-CREATE TABLE IF NOT EXISTS `users` (
+<?php 
+// post.php
+session_start();
+ini_set('display_errors', 'On');
 
-`username` CHAR(100) NOT NULL,
-`firstname` CHAR(100) NOT NULL,
-`lastname` CHAR(100) NOT NULL,
-`age` INT(10) NOT NULL,
-`email` CHAR(100) NOT NULL,
-`gender` CHAR(1) NOT NULL,
-`passhash` CHAR(100) NOT NULL,
+include("common.php");
+include("db.php");
+include("navbar.php");
 
-PRIMARY KEY(`username`)
+if(!isset($_SESSION['username']))
+{
+  header('Location: login.php');
+}
+else
+{
 
-);
+    $now = time(); // checking the time now when home page starts
 
-CREATE TABLE IF NOT EXISTS `cars` (
+    if($now > $_SESSION['expire'])
+    {
+        session_destroy();
+        header('Location: login.php');
+    }
+}
+// Check if form is getting submitted
+if (!isset($_POST['submitpost'])):
+    // Display the user post form
+	
+?>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="icon" 
+      type="image/png" 
+      href="favicon.png">
+	
+	<title>Create a carpool</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<!--[if lt IE 7]>
+		<link rel="stylesheet" type="text/css" href="style-ie.css" />
+	<![endif]-->
+</head>
 
-`car_id` INT(50) NOT NULL AUTO_INCREMENT,
-`cartype` CHAR(50),
-`neatness` CHAR(50),
-`color` CHAR(50),
-`seats` INT(50),
+<body>
 
-PRIMARY KEY(`car_id`)
+	<div id="page-wrap">
+		<div id="inside">
+			<div id="botright">
+			<a href="http://www.chris-schuster.com/" target="_blank" style="color:#FFFFFF;">image credit</a>
+			</div>
+			
+			<div id="header">
+				<img src="header.png">
+			</div>
+		
+			<div id="left-sidebar">
+				<?php
+					if (isset($_SESSION['username']) == 1)
+						insertnavbar(1,1);
+					else
+						insertnavbar(0,1);
+				?>
 
-);
+			</div>
+	
+			<div id="main-content">
+						
+			<h2>Create carpool</h2>
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+			<select name="type">
+			<option>Add</option>
+			<option>Delete</option>
+			<option>Edit</option>
+			</select>
+			<br>
+			Carpool ID:<br />
+			<input type="text" name="carpoolid" size="50" /><br />
 
-CREATE TABLE IF NOT EXISTS `carpool` (
 
-`car_id` INT(50),
-`carpool_id` INT(50) NOT NULL AUTO_INCREMENT,
-`datetime` CHAR(50),
-`duration` CHAR(50),
-`numberofpassengers` INT(10),
-`startingtime` CHAR(50) NOT NULL,
-`endingtime` CHAR(50) NOT NULL,
-`recurrencelevel` INT(10),
-`startinglocation_id` INT(100),
-`endinglocation_id` INT(100),
+			Start time:<br />
+			<input type="text" name="start" size="50" /><br />
+			
+			End time:<br />
+			<input type="text" name="end" size="50" /><br />
+			
+			Date:<br />
+			<input type="text" name="date" size="50" /><br />
+			
+			Duration:<br />
+			<input type="text" name="duration" size="50" /><br />
+			
+			Number of passengers:<br />
+			<input type="text" name="numpass" size="50" /><br />
+			
+			Car ID:<br />
+			<input type="text" name="carid" size="50" /><br />
+			
+			Recurrence level (frequent or one-time):<br />
+			<input type="text" name="recur" size="50" /><br />
+
+			<br/> <br/>
+			
+			<div id = "c">
+			
+			<div id = "leftlocat">
+			
+				Starting Location<br />
+				
+				Address<br />
+				<input type="text" name="addressS" size="50" /><br />
+			
+				City:<br />
+				<input type="text" name="cityS" size="50" /><br />
+
+				State:<br />
+				<input type="text" name="stateS" size="50" /><br />
+
+			</div>
+			
+			<div id = "rightlocat">
+			
+				Ending Location<br />
+				
+				Address<br />
+				<input type="text" name="addressE" size="50" /><br />
+			
+				City:<br />
+				<input type="text" name="cityE" size="50" /><br />
+
+				State:<br />
+				<input type="text" name="stateE" size="50" /><br />
+
+			</div>
+			
+			</div>
+
+			<div style="clear: both;"></div>
+			
+			<input type="submit" name="submitpost" value="Create carpool" />
+			</form>
+			
+			<br />
+			
+			<form method="post" action="index.php">
+			
+			<input type ="submit" value = "cancel" />
+			
+			</form>
+			
+			
+			</div>
+			
+
+			
+		</div>
+		
+		<div style="clear: both;"></div>
+	
+	</div>
+	
+	
+	
+
+</body>
+
+</html>
+
+<?php
+else:
+// Process post submission
+dbConnect('nejadb-db');
+if ($_POST['carpoolid']=='') 
+{
+    error('One or more required fields were left blank.\\n'.'Please fill them in and try again.');
+}
+
+$type = mysql_real_escape_string(trim($_POST['type']));
+
+$carpoolid=mysql_real_escape_string(trim($_POST['carpoolid']));
+$start=mysql_real_escape_string(trim($_POST['start']));
+$end=mysql_real_escape_string(trim($_POST['end']));
+$duration=mysql_real_escape_string(trim($_POST['duration']));
+$date=mysql_real_escape_string(trim($_POST['date']));
+$carid=mysql_real_escape_string(trim($_POST['carid']));
+$numpass=mysql_real_escape_string(trim($_POST['numpass']));
+$recur=mysql_real_escape_string(trim($_POST['recur']));
+$addressS = mysql_real_escape_string(trim($_POST['addressS']));
+$addressE = mysql_real_escape_string(trim($_POST['addressE']));
+$cityS = mysql_real_escape_string(trim($_POST['cityS']));
+$cityE = mysql_real_escape_string(trim($_POST['cityE']));
+$stateS = mysql_real_escape_string(trim($_POST['stateS']));
+$stateE = mysql_real_escape_string(trim($_POST['stateE']));
+$datetime=date("d/m/y h:i");
+$author=$_SESSION['username'];
+
+if($type == "Add"){
+    
+	$query = "INSERT INTO startinglocation (city, state, address) VALUES ('$cityS', '$stateS', '$addressS')";
+	if(!mysql_query($query))
+		error("Couldn't create a startinglocation");
+	$stl = mysql_insert_id();
+
+	error("$stl");
+	
+	$query = "INSERT INTO endinglocation (city, state, address) VALUES ('$cityE', '$stateE', '$addressE')";
+	if(!mysql_query($query))
+		error("Couldn't create an endinglocation");
+	$endl = mysql_insert_id();
+	
+		
+	$query = "INSERT INTO carpool (startingtime, endingingtime, datetime, duration, car_id, numberofpassengers, recurrencelevel, startinglocation_id, endinglocation_id) VALUES ('$start', '$end', '$date', '$duration', '$carid', '$numpass', '$recur', '$stl', '$endl')";
+	
+	if(!mysql_query($query))
+		error("Couldn't create a carpool");
+	
+}elseif($type == "Delete")
+	$query="DELETE FROM carpool WHERE carpool_id = '$carpoolid'";
+elseif($type == "Edit")
+	$query="UPDATE carpool SET startingtime = '$start' endingtime = 'end' datetime = '$date' duration = '$duration' car_id = '$carid' numberofpassengers = '$numpass' recurrencelevel = '$recur' WHERE carpool_id = '$carpoolid'";
 
 
-PRIMARY KEY(`carpool_id`),
-FOREIGN KEY(`car_id`) REFERENCES cars ON DELETE SET NULL ON UPDATE CASCADE,
-FOREIGN KEY(`startinglocation_id`) REFERENCES startinglocation ON DELETE SET NULL ON UPDATE CASCADE,
-FOREIGN KEY(`endinglocation_id`) REFERENCES endinglocation ON DELETE SET NULL ON UPDATE CASCADE
 
-);
-
-CREATE TABLE IF NOT EXISTS `driver` (
-
-`username` CHAR(50) NOT NULL,
-`carpool_id` INT(50) NOT NULL,
-
-FOREIGN KEY (`username`) REFERENCES account ON DELETE SET NULL ON UPDATE CASCADE,
-FOREIGN KEY (`carpool_id`) REFERENCES carpool ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS `passenger` (
-
-`username` CHAR(50) NOT NULL,
-`carpool_id` INT(50) NOT NULL,
-
-FOREIGN KEY (`username`) REFERENCES account ON DELETE SET NULL ON UPDATE CASCADE,
-FOREIGN KEY (`carpool_id`) REFERENCES carpool ON DELETE SET NULL ON UPDATE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS `startinglocation` (
-
-`startinglocation_id` INT(100) NOT NULL AUTO_INCREMENT,
-`address` VARCHAR(200) NOT NULL,
-`zipcode` CHAR(50) NOT NULL,
-`city` CHAR(50) NOT NULL,
-`state` CHAR(50) NOT NULL,
-`longitude` FLOAT(10) NOT NULL,
-`latitude` FLOAT(10) NOT NULL,
-
-PRIMARY KEY(`startinglocation_id`)
-
-);
-
-CREATE TABLE IF NOT EXISTS `endinglocation` (
-
-`endinglocation_id` INT(100) NOT NULL AUTO_INCREMENT,
-`address` VARCHAR(200) NOT NULL,
-`zipcode` CHAR(50) NOT NULL,
-`city` CHAR(50) NOT NULL,
-`state` CHAR(50) NOT NULL,
-`longitude` FLOAT(10) NOT NULL,
-`latitude` FLOAT(10) NOT NULL,
-
-PRIMARY KEY(`endinglocation_id`)
-);
-
+mysql_close();
+header('Location: success.php');
+?>
+<?php
+endif;
+?>
